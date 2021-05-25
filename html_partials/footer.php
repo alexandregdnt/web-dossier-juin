5f5f1ada5a332a68435ffe599a1f47faa279e265
Arpolex <alexandregrodent@gmail.com>
2021-05-25 10:06:00 +0200

    :bug: :package: :iphone: Fix jquery and work on responsive design

--- html_partials/footer.php
+++ html_partials/footer.php
@@ -1,5 +1,5 @@
 <!-- Footer -->
-<footer class="py-5 bg-dark">
+<footer class="py-3 bg-dark fixed-bottom">
     <div class="container">
         <p class="m-0 text-center text-white">Copyright &copy; <?= date("Y"); ?> <a href="https://alexgr.be" target="_blank">Alexandre Grodent</a></p>
     </div>
@@ -7,7 +7,10 @@
 </footer>
 
 <!-- Bootstrap core JavaScript -->
+<script src="./assets/vendor/jquery/jquery.min.js"></script>
 <script src="./assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
+<!-- Plugin JavaScript -->
+<script src="./assets/vendor/jquery-easing/jquery.easing.min.js"></script>
 </body>
 </html>
